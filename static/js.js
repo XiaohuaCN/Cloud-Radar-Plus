e3a8d9bb8b4d44b191023871bcacf2dcb6db1503
XiaohuaCN <38711801+XiaohuaCN@users.noreply.github.com>
2018-04-27 20:03:46 +0800

    XiaohuaCN
    XiaohuaCN

---
+++ static/js.js
@@ -0,0 +1,15 @@
+﻿document.getElementById("menuBox").onclick=function(){slide()};
+
+function slide(){
+	var box1 = document.getElementById('navBox');
+	var box2 = document.getElementById("menuBox");
+	 if(box1.style.display=="none"){
+            box1.style.display="block"; 
+             box2.style.left="340px";
+                             //显示
+        }else{
+            box1.style.display="none"; 
+            box2.style.left='0px';                 //隐藏
+       } 
+	
+}
